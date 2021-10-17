ddf8c9c11e31f31559ebc561f646a5c4f75418d0
David Angulo <angulo.davi@gmail.com>
2021-10-17 10:56:57 +0100

    cmake changes

--- pyWrap/animal/__init__.py
+++ pyWrap/animal/__init__.py
@@ -1,3 +1,6 @@
-from ._animal import *
-from .animal import *
+from ._animal import Animal
 from .animal_greetings import animal_greetings
+
+Animal.__str__ = lambda self: self.greetings()
+
+Animal.__repr__ = lambda self: self.greetings()
--- pyWrap/animal/animal.py
+++
@@ -1,5 +0,0 @@
-from ._animal import Animal as Animal
-
-Animal.__str__ = lambda self: self.greetings()
-
-Animal.__repr__ = lambda self: self.greetings()
